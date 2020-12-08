84b5aba8c5a031ee81109bef480f39d2d2c9a3ad
Caio Ueno <caioluiggy@hotmail.com>
2020-12-08 20:13:49 -0300

    removing not used variables

--- prolog-atom-count/shared_elements.pl
+++ prolog-atom-count/shared_elements.pl
@@ -1,13 +1,13 @@
 % database
 
-belongs_to(Elem, [Elem|Y]).
-belongs_to(Elem, [X|Y]) :- belongs_to(Elem, Y).
+belongs_to(Elem, [Elem|_]).
+belongs_to(Elem, [_|Y]) :- belongs_to(Elem, Y).
 
 concat([], X, X).
 concat([A|B], X, [A|Y]) :- concat(B, X, Y). 
 
-shared_elements([], C, []).
+shared_elements([], _, []).
 shared_elements([A|B], C, [A|D]) :- atomic(A), belongs_to(A, C), shared_elements(B, C, D), !.
-shared_elements([A|B], C, D) :- shared_elements(B, C, D), !.
+shared_elements([_|B], C, D) :- shared_elements(B, C, D), !.
 
 concat_shared_elements(A, B, C) :- shared_elements(A, B, X), shared_elements(B, A, Y), concat(X, Y, C).
