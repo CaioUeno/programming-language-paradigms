2894fbaa056e7722d0e785899e757aa1a25096f3
Caio Ueno <caioluiggy@hotmail.com>
2020-12-15 20:42:41 -0300

    final version

--- prolog-atom-count/Main.pl
+++ prolog-atom-count/Main.pl
@@ -1,6 +1,7 @@
 empty([]).
 removeSubList([],[]):- !.
-removeSubList([X|Y],R):- is_list(X), not(empty(X)), removeSubList(X,Z), removeSubList(Y,W), append(Z,W,R), !.
+removeSubList([X|Y],R):- is_list(X), not(empty(X)), removeSubList(X,Z),
+                         removeSubList(Y,W), append(Z,W,R), !.
 removeSubList([X|Y],[X|Z]):- removeSubList(Y,Z).
 
 removeVars([],[]):- !.
@@ -22,4 +23,10 @@ removeAll(Elem,[X|T1],[X|T2],N):- removeAll(Elem,T1,T2,N).
 countElem([],[]):- !.
 countElem([X|T],[[X,N]|C]):- removeAll(X,[X|T],L,N), countElem(L,C).
 
-countAtoms(L1, L2, Lout):- removeSubList(L1,L1rem), removeSubList(L2,L2rem), removeVars(L1rem,L1mod), removeVars(L2rem,L2mod), concatSharedElements(L1mod, L2mod, L), countElem(L,Lout).
+conta_atomos(L1, L2, Lout):- removeSubList(L1,L1rem), removeSubList(L2,L2rem),
+                             removeVars(L1rem,L1mod), removeVars(L2rem,L2mod), concatSharedElements(L1mod, L2mod, L), countElem(L,Lout).
+
+conta_atomos_read(L1, L2, Lout):- read(L1), read(L2), conta_atomos(L1, L2, Lout).
+
+
+
